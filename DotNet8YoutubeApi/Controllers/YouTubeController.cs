d9baef6547deb8d2404b5a0ca86ca93cc7c73ff6
Rafael Santos <rafa.05125@gmail.com>
2024-03-14 14:12:19 -0300

    YouTubeController

---
+++ DotNet8YoutubeApi/Controllers/YouTubeController.cs
@@ -0,0 +1,30 @@
+﻿using Google.Apis.Services;
+using Google.Apis.YouTube.v3;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet8YoutubeApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class YouTubeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetChannelVideos()
+        {
+            var youtubeService = new YouTubeService(new BaseClientService.Initializer
+            {
+                ApiKey = "",
+                ApplicationName = "MyYoutubeApp"
+            });
+
+            var searchRequest = youtubeService.Search.List("snippet");
+            searchRequest.ChannelId = "UCq8LldVrjqe61KQttZlLW8g";
+            searchRequest.Order = SearchResource.ListRequest.OrderEnum.Date;
+
+            var searchResponse = await searchRequest.ExecuteAsync();
+
+            return Ok(searchResponse);
+        }
+    }
+}
